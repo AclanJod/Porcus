8a5e2d99c797f0d71b9734c77d929b6c952bf7a8
vincentrondero <vincentpatrickrondero@gmail.com>
2023-07-05 17:33:11 +0800

    mobile_app

---
+++ mobile_app/src/app/tab3/tab3.page.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+  constructor() {}
+
+
+  div1:boolean=true;
+  div2:boolean=false;
+
+
+  div1Function(){
+      this.div1=true;
+      this.div2=false;
+  }
+
+  div2Function(){
+      this.div2=true;
+      this.div1=false;
+  }
+
+
+}
+
+
+
