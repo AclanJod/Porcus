8162febff174d63219919ffe48ed3af3303a8797
JasperSalazar <20-05470@g.batstate-u.edu.ph>
2023-07-18 13:03:54 +0800

    documentation

--- server/routes/tasks_routes.js
+++
@@ -1,8 +0,0 @@
-const taskController = require("../controllers/tasksController");
-
-const router = require("express").Router();
-
-router.post("/addTask", taskController.addTasks);
-router.get("/getTask", taskController.getAllTasks);
-
-module.exports = router;
--- server/routes/users_routes.js
+++
@@ -1,8 +0,0 @@
-const userController = require("../controllers/usersController");
-
-const router = require("express").Router();
-
-router.post("/register", userController.addUsers);
-router.get("/display", userController.getAllUsers);
-
-module.exports = router;
